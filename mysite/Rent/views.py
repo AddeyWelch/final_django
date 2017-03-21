a58892110b67ef6d359cb9345b7f4f7510712a2d
Addey Welch <awelch@basecampcodingacademy.org>
2017-03-21 09:20:35 -0500

    Added urls

--- mysite/Rent/views.py
+++ mysite/Rent/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def home(request):
+    return render(request, 'rent/home.html')
+
+
+def rental(request):
+    return render(request, 'rent/rental.html')
+
+
+def contact(request):
+    return render(request, 'rent/contact.html')
