f71bb0f8b7e16450c4fe353dce5fe5308a914e5e
Addey Welch <awelch@basecampcodingacademy.org>
2017-03-24 16:09:52 -0500

    Adding data to the database. (unfinished)

--- mysite/Rent/views.py
+++ mysite/Rent/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from .models import Bounce
 
 # Create your views here.
 
@@ -8,9 +9,21 @@ def home(request):
 
 
 def rental(request):
+    # context to a variable Ex. {'Name': Name}
     print('This worked')
     return render(request, 'Rent/rental.html')
 
 
 def contact(request):
     return render(request, 'Rent/contact.html')
+
+
+def view_name(request, url, quantity, price, description):
+    all_bounces = Bounce.objects.all()
+    context = {'all_bounces': all_bounces}
+    # context = {
+    #     'Quantity': 20,
+    #     'Prices': [100, 175],
+    #     'Description':
+    # }
+    return render(request, 'rental.html', context)
