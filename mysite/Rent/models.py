dcb3d32c07698f043a45d6bd8767adf04aef471a
Addey Welch <awelch@basecampcodingacademy.org>
2017-03-24 13:38:29 -0500

    Added model and removed JS file

--- mysite/Rent/models.py
+++ mysite/Rent/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
+
 # Create your models here.
+class Bounce(models.Model):
+    name = models.CharField(max_length=30)
+    quantity = models.IntegerField()
+    price = models.IntegerField()
+    description = models.CharField(max_length=100)
